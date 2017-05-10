5e7d644f627799e3d8923c5d7440abe8a6ae68af
BarberousseBinks <louis.dhanis@student.umons.ac.be>
2017-05-10 22:26:10 +0200

    New updates -> Ant & stuff

--- SpeedyRoadie/src/frontend/Main.java
+++ SpeedyRoadie/src/frontend/Main.java
@@ -20,8 +20,20 @@ public class Main {
      * @throws java.lang.InterruptedException
      */
     public static void main(String[] args) throws IOException, InterruptedException {
-
-		GuiFrame game = new GuiFrame();
+		
+		if(args.length == 0){ //S'il n'y a pas d'arguments
+			GuiFrame game = new GuiFrame();
+		}
+		else if(args.length == 3){
+			
+		}	
+		else{
+			String argChain = "";
+			for(int i = 0; i < args.length; i++){
+				argChain = argChain+ " " + args[i];
+			}
+			System.out.println("Argument manquant: \n votre requête > java frontend/Main "+ argChain +" \n requête standardisée > java frontend/Main input.xsb input.mov output.xsb \n - input.xsb chemin vers un fichier xsb de base \n - input.mov chemin vers le fichier mov à appliquer à l'input \n - output.mov chemin vers le fichier de sortie");
+		}
 
     }
     
